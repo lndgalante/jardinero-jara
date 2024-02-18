0538bf4a137ac51831ff3b95fa3fe056c9c28cd7
Leonardo Galante <lndgalante@gmail.com>
2024-02-18 20:55:39 -0300

    feat: Finish MVP

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+
+
+import { nanoid } from "nanoid";
+import { FaWhatsapp } from "react-icons/fa";
+import { GiBamboo, GiCycle, GiGardeningShears, GiGrass, GiHighGrass, GiPlantWatering } from "react-icons/gi";
+
+import { Button, Badge, Callout, Link } from "@radix-ui/themes";
+import { ReactCompareSlider, ReactCompareSliderImage } from "react-compare-slider";
+
+// images
+import sampleOneAfter from './assets/sample-1-after.webp'
+import sampleOneBefore from './assets/sample-1-before.webp'
+
+function CompareSliderImageWrapper({ time, children }: { time: 'before' | 'after', children: React.ReactNode }) {
+  return (
+    <div className="relative h-full">
+      <Badge color={time === 'before' ? 'tomato' : 'green'} className="absolute top-2 left-2" size="2" variant="surface">
+        {time === 'before' ? 'Antes' : 'Después'}
+      </Badge>
+      {children}
+    </div>
+  );
+}
+
+// constants
+const services = [
+  { label: 'Cortes de césped', icon: <GiHighGrass className="w-6 h-6" /> },
+  { label: 'Podas', icon: <GiGardeningShears className="w-6 h-6" /> },
+  { label: 'Limpieza de yuyos', icon: <GiGrass className="w-6 h-6" /> },
+  { label: 'Diseño de espacios verdes', icon: <GiBamboo className="w-6 h-6" /> },
+  { label: 'Mantenimiento de jardines', icon: <GiCycle className="w-6 h-6" /> },
+  { label: 'Asesoramiento para tus plantas', icon: <GiPlantWatering className="w-6 h-6" /> },
+].map(service => ({ ...service, id: nanoid() }))
+
+// utils
+function generateWhatsAppLink() {
+  const phone = '543442604355'
+  const message = 'Hola 👋, te quería consultar por los servicios de jardinería 🪴'
+
+  return `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`
+
+}
+
+const whatsappLink = generateWhatsAppLink()
+
+export default function MyApp() {
+  return (
+    <main className="min-h-screen md:h-screen w-full p-2 bg-green-950 flex items-center">
+      <section className="container flex flex-col lg:flex-row gap-4 m-auto max-h-[720px]">
+        <aside className="w-full lg:max-w-xs gap-4 lg:gap-0 flex flex-col justify-between bg-green-50 p-3 rounded-md">
+          <header className="text-center">
+            <h1 className="text-3xl mb-2">Jardinero Jara</h1>
+            <h2 className="text-lg uppercase text-balance">Cuidado y Mantenimiento de Espacios Verdes</h2>
+          </header>
+
+          <div className="flex flex-col gap-2">
+            {services.map(service => (
+              <Callout.Root key={service.id} variant="surface" size="1">
+                <Callout.Icon>
+                  {service.icon}
+                </Callout.Icon>
+                <Callout.Text>
+                  {service.label}
+                </Callout.Text>
+              </Callout.Root>
+            ))}
+          </div>
+
+          <Link href={whatsappLink} rel="noopener noreferrer" target="_blank" className="w-full" >
+            <Button size="4" className="w-full !cursor-pointer">
+              <FaWhatsapp className="w-6 h-6" /> Contactar
+            </Button>
+          </Link>
+        </aside>
+
+
+        <article className="bg-green-50 p-3 rounded-md">
+          <ReactCompareSlider
+            position={90}
+            style={{
+              width: '100%',
+              height: '100%',
+              borderRadius: '0.375rem'
+            }}
+            itemOne={
+              <CompareSliderImageWrapper time="before">
+                <ReactCompareSliderImage src={sampleOneBefore} alt="Antes" className="rounded-md" />
+              </CompareSliderImageWrapper>
+            }
+            itemTwo={
+              <CompareSliderImageWrapper time="after">
+                <ReactCompareSliderImage src={sampleOneAfter} alt="Después" className="rounded-md" />
+              </CompareSliderImageWrapper>
+            }
+          />
+        </article>
+      </section>
+    </main>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Theme } from "@radix-ui/themes";
+
+// styles
+import "./tailwind.css"
+import "@radix-ui/themes/styles.css";
+
+// internals
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Theme accentColor="green" grayColor="gray" panelBackground="solid" scaling="110%" radius="large">
+      <App />
+    </Theme>
+  </React.StrictMode>,
+);
